d5118678cadc0eabc78060cff4f54476e9660ce5
Brian Shepppard <slashdashbrian@gmail.com>
2023-04-01 23:16:45 +0200

    Start of the app

---
+++ src/components/CryptoCurrencies.jsx
@@ -0,0 +1,63 @@
+import { React, useState, useEffect } from "react";
+import millify from "millify";
+import { Link } from "react-router-dom";
+import { Card, Row, Col, Input } from "antd";
+
+import { useGetCryptosQuery } from "../services/cryptoApi";
+
+export const CryptoCurrencies = ({ simplified }) => {
+  const count = simplified ? 10 : 100;
+
+  const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
+
+  const [SearchTerm, setSearchTerm] = useState("");
+
+  const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+
+  useEffect(() => {
+    const filteredCryptos = cryptosList?.data?.coins.filter((coin) =>
+      coin.name.toLowerCase().includes(SearchTerm.toLowerCase())
+    );
+
+    setCryptos(filteredCryptos);
+  }, [cryptosList, SearchTerm]);
+
+  if (isFetching) return "Loading...";
+
+  return (
+    <>
+      <div className="search-crypto">
+        <Input
+          placeholder="Search Cryptocurrency"
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+
+      <Row gutter={[32, 32]} className="crypto-card-container">
+        {cryptos?.map((currency) => (
+          <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
+            <Link to={`/crypto/${currency.id}`}>
+              <Card
+                hoverable
+                title={`${currency.rank}. ${currency.name}`}
+                extra={
+                  <img
+                    className="crypto-image"
+                    src={currency.iconUrl}
+                    alt={`${currency.title} icon`}
+                  ></img>
+                }
+              >
+                <p>Price: {millify(currency.price)}</p>
+                <p>Market Cap: {millify(currency.marketCap)}</p>
+                <p>Daily change: {millify(currency.change)}%</p>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+
+export default CryptoCurrencies;
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,25 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+  "X-RapidAPI-Key": "6504660259msh05fecda2ba5afacp1e0397jsn8dc9945058c6",
+  "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+const createRequest = (url) => ({
+  url,
+  headers: cryptoApiHeaders,
+});
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest("/coins?limit=" + count),
+    }),
+  }),
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
